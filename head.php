26f7cbd6c9e1a369d0d38dd5f3d0cdbe49814506
Aline <aline@Alines-MacBook-Pro.local>
2016-10-25 19:27:13 -0400

    fixed path to css stylesheets

--- head.php
+++ head.php
@@ -7,7 +7,7 @@
         <meta name = "description" content= "The solution for teachers. This application helps teachers to efficiently
         seating charts.">
         <meta name = "keywords" content="teacher, seating chart, seat, assign seats, students">
-        <link rel = "stylesheet" href = "style.css">
-        <link rel = "stylesheet" href = "menustyle.css">
+        <link rel = "stylesheet" href = "css/style.css">
+        <link rel = "stylesheet" href = "css/menustyle.css">
         <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     </head>
