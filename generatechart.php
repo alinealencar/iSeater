6a62a65c07518154a42b082c577a44c5891f2c37
kogiya <kie.ogiya@gmail.com>
2016-11-03 20:03:58 -0400

    modify html/css of index, generatechart, menu
    Made size of all img files of icons in generatechart page same.
    Deleted border around the img files and included it in img.

--- generatechart.php
+++ generatechart.php
@@ -12,11 +12,11 @@ require "menu.php";
             <option value="2">2nd grade</option>
             <option value="3">3rd grade</option>
         </select>
-        <br><br>
+        <br><br><br><br>
 
         <span class = "formSection">Layout</span>
 
-        <br><br>
+        <br><br><br>
         <span class = "formLabel"><strong>Single Column</strong> (columns x rows (max seats))</span>
         <br><br>
         <div class = "layoutGroup">
@@ -49,7 +49,7 @@ require "menu.php";
             </div>
         </div>
 
-        <br><br>
+        <br><br><br>
 
         <span class = "formLabel"><strong>Double Column</strong> (columns x rows (max seats))</span>
         <br><br>
@@ -67,7 +67,7 @@ require "menu.php";
                 <input type="radio" name="layout" value="fourfive" checked>
             </div>
         </div>
-        <br><br>
+        <br><br><br>
 
         <span class = "formLabel"><strong>Custom Layout</strong> (Select column type and input column and rows)</span>
         <br><br>
@@ -76,12 +76,14 @@ require "menu.php";
         <br><br>
         <span class = "formLabel">Columns: </span><input type = "text" name = "numColumns">&nbsp;
         <span class = "formLabel">Rows: </span><input type = "text" name = "numRows">
-        <br><br><br>
+
+        <br><br><br><br><br>
+
         <span class = "formSection">Gender Pattern</span>
         <br><br>
         <div class = "genderGroup">
             <div class = "genderBox">
-                <div class = "gender"><img alt="no gender" src="images/nogender.png"></div><br>
+                <div class = "gender"><img alt="no gender" src="images/nogenderf.png"></div><br>
                 <span class = "genderSelection">No Restriction</span><br>
                 <input type="radio" name="gender" value="nogender" checked>
             </div>
@@ -96,49 +98,53 @@ require "menu.php";
                 <input type="radio" name="gender" value="boysgirls" checked>
             </div>
             <div class = "genderBox">
-                <div class = "gender"><img alt="alternated" src="images/everyother.png"></div><br>
+                <div class = "gender"><img alt="alternated" src="images/everyotherf.png"></div><br>
                 <span class = "genderSelection">Alternated</span><br>
                 <input type="radio" name="gender" value="alternated" checked>
             </div>
         </div>
-        <br>
+
+        <br><br><br><br><br>
+
         <span class = "formSection">Order</span>
         <br><br>
         <div class = "orderGroup">
             <div class="orderBox">
-                <div class = "order"><img alt="alphabetical horizontal" src="images/alphabeticalhorizontal.png"></div><br>
-                <span class = "genderSelection">Alphabetical Order (Horizontal)</span><br>
+                <div class = "order"><img alt="alphabetical horizontal" src="images/alphabeticalhorizontalf.png"></div><br>
+                <span class = "genderSelection">Alphabetical Order<br>(Horizontal)</span><br>
                 <input type="radio" name="order" value="alphabetical" checked>
             </div>
             <div class="orderBox">
-                <div class = "order"><img alt="alphabetical vertical" src="images/alphabeticalvertical.png"></div><br>
-                <span class = "genderSelection">Alphabetical Order (Vertical)</span><br>
+                <div class = "order"><img alt="alphabetical vertical" src="images/alphabeticalverticalf.png"></div><br>
+                <span class = "genderSelection">Alphabetical Order<br>(Vertical)</span><br>
                 <input type="radio" name="order" value="alphabetical" checked>
             </div>
             <div class="orderBox">
-                <div class = "order"><img alt="id horizontal" src="images/idhorizontal.png"></div><br>
-                <span class = "genderSelection">ID Order (Horizontal)</span><br>
-                <input type="radio" name="order" value="byid" checked>
+                <div class = "order"><img alt="alphabetical horizontal" src="images/randomf.png"></div><br>
+                <span class = "genderSelection">Random Order<br></span><br>
+                <input type="radio" name="order" value="random" checked>
             </div>
         </div>
         <br><br>
         <div class = "ordergroup">
             <div class="orderBox">
-                <div class = "order"><img alt="alphabetical horizontal" src="images/idvertical.png"></div><br>
+                <div class = "order"><img alt="id horizontal" src="images/idhorizontalf.png"></div><br>
+                <span class = "genderSelection">ID Order<br>(Horizontal)</span><br>
+                <input type="radio" name="order" value="byid" checked>
+            </div>
+            <div class="orderBox">
+                <div class = "order"><img alt="alphabetical horizontal" src="images/idverticalf.png"></div><br>
                 <span class = "genderSelection">ID Order<br>(Vertical)</span><br>
                 <input type="radio" name="order" value="byid" checked>
             </div>
             <div class="orderBox">
-                <div class = "order"><img alt="alphabetical horizontal" src="images/random.png"></div><br>
-                <span class = "genderSelection">Random Order</span><br>
-                <input type="radio" name="order" value="random" checked>
-            </div>
-            <div class="orderBox">
-                <div class = "order"><img alt="alphabetical horizontal" src="images/manual.png"></div><br>
-                <span class = "genderSelection">Manual Order</span><br>
+                <div class = "order"><img alt="alphabetical horizontal" src="images/manualf.png"></div><br>
+                <span class = "genderSelection">Manual Order<br></span><br>
                 <input type="radio" name="order" value="manual" checked>
             </div>
-            <br><br>
+
+            <br><br><br><br><br>
+
             <input class = "submitButton" type="submit" value="Generate Chart">
         </div>
     </form>
--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <nav>
     <img alt="iSeater logo" src="images/logo2.png">
-    <span class = "signedin greyFont">Signed in as <?php echo $username ?>.</span>
+    <span class = "signedin greyFont">Signed in as <?php echo $username ?></span>
     <ul class = "menu">
         <li><a class = "greyFont menuItem" href="#">Students</a></li>|
         <li><a class = "greyFont menuItem" href="#">Generate Chart</a></li>
-        <li class = "logout"><a class = "whiteFont" href="#">Log Out</a></li>
+        <li class = "signout"><a class = "whiteFont" href="#">Sign Out</a></li>
     </ul>
 </nav>
 
