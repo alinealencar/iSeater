a0bd4f6f02a99019ba333584705c59420a4ee3d8
Princess Ilasin <princess.ilasin@georgebrown.ca>
2016-11-15 01:22:29 -0500

    Add files via upload

---
+++ teacherspage.php
@@ -0,0 +1,21 @@
+<?php
+$title = "iSeater - Generate Chart";
+$username = "Aline";
+require "head.php";
+require "menu.php";
+?>
+
+
+<body>
+    <div id="left">
+        <h3>Buttons here</h3>
+    </div>
+
+    <div id="right">
+        <h3>Something here</h3>
+    </div>
+</body>
+
+<?php
+require "footer.php";
+?>
