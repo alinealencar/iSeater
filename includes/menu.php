f14cd5554e98b02cce8ce9b9a08cd9d9c07b7b39
Aline <aline@Alines-MacBook-Pro.local>
2016-11-16 23:32:50 -0500

    organized directories

---
+++ includes/menu.php
@@ -0,0 +1,11 @@
+<nav>
+    <span class = "signedin greyFont">Signed in as <?php echo $user ?></span>
+    <img alt="iSeater logo" src="../images/logo2.png">
+    <ul class = "menu">
+        <li><a class = "greyFont menuItem" href="#">Students</a></li>|
+        <li><a class = "greyFont menuItem" href="#">Generate Chart</a></li>|
+        <li><a class = "greyFont menuItem" href="#">My Account</a></li>
+        <li class = "signout"><a class = "whiteFont" href="#">Sign Out</a></li>
+    </ul>
+</nav>
+
