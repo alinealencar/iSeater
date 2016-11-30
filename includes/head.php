ddd7706489f1dbf448fbab98318e2a81bbd4f494
Aline <aline@Alines-MacBook-Pro.local>
2016-11-30 12:51:31 -0500

    removed sorttable.js and dashboard.css from head.php

--- includes/head.php
+++ includes/head.php
@@ -9,12 +9,10 @@
         <meta name = "keywords" content="teacher, seating chart, seat, assign seats, students">
         <link rel = "stylesheet" href = "css/style.css">
         <link rel = "stylesheet" href = "css/menustyle.css">
-        <link rel = "stylesheet" href = "css/dashboard.css">
         <link rel = "stylesheet" href = "css/formstyle.css">
         <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One|Baloo+Paaji|Raleway" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel = "stylesheet" href = "css/custom.css">
-        <script src = "../js/sorttable.js" type = "text/javascript"></script>
         <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
         <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </head>
