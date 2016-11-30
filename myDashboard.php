ccd9333e5d10594bc90952df22808f1e92d97377
Aline <aline@Alines-MacBook-Pro.local>
2016-11-30 09:42:57 -0500

    fixed mydashboard css

--- myDashboard.php
+++ myDashboard.php
@@ -4,51 +4,45 @@ $username = "Aline";
 require "includes".DIRECTORY_SEPARATOR."head.php";
 require "includes".DIRECTORY_SEPARATOR."menu.php";
 require "includes".DIRECTORY_SEPARATOR."databaseConnection.php";
-
 ?>
 <body>
-    <script>
-        function showThisChart(obj, className){
-            $("table").hide();
-            $(".titleBar").hide();
-            $("." + className + "").show();
+<script>
+    function showThisChart(obj, className){
+        $("table").hide();
+        $(".titleBar").hide();
+        $("." + className + "").show();
+    }
+</script>
+<div class = "container">
+        <div class = ".col-md-5" id="left"><br>
+            <h3> Classes: </h3>
+            <br>
+            <?php
+            $query = 'SELECT ClassID FROM Class';
+            $result = $conn -> query($query);
+            $buttonsStr = "";
+            if($result->num_rows){
+                while($row  = $result->fetch_assoc())
+                    $buttonsStr .= "<button onclick = \"showThisChart(this, '".$row["ClassID"]."')\" type='button' class='btn btn-info ".$row["ClassID"]."'>".$row["ClassID"]."</button><br><br><br>";
+            }
+            echo $buttonsStr;
+            ?>
+        </div>
 
-        }
-    </script>
-    <div class = "container">
-        <div class = "row">
-            <div class = ".col-md-5">
-                <h3> Classes: </h3>
-                <br>
-                <?php
-                    $query = 'SELECT ClassID FROM Class';
-                    $result = $conn -> query($query);
-                    $buttonsStr = "";
-                    if($result->num_rows){
-                        while($row  = $result->fetch_assoc())
-                            $buttonsStr .= "<button onclick = \"showThisChart(this, '".$row["ClassID"]."')\" type='button' class='btn btn-info ".$row["ClassID"]."'>".$row["ClassID"]."</button><br>";
-                    }
-                    echo $buttonsStr;
-                ?>
-            </div>
-
-            <div class = ".col-md-5">
-                <?php
-                $query = 'SELECT * FROM Class;';
-                $result = $conn -> query($query);
-
-                $chartResult = "";
-                if($result->num_rows) {
-                    while($row = $result->fetch_assoc()){
-                        $chartResult .= "<div class = 'titleBar ".$row['ClassID']."' style = 'display:none;'><hr>";
-                        $chartResult .= "<span>Class ID: " . $row['ClassID'] . " | Class Name: " . $row['ClassName'] . " | Term: " . $row['Term'] . '</span><br>';
-                        $chartResult .= "<hr></div><br><br>";
-                        $serializedData = $row['Layout'];
+        <div class = ".col-md-5" id="right">
+            <?php
+            $query = 'SELECT * FROM Class;';
+            $result = $conn -> query($query);
+            $chartResult = "";
+            if($result->num_rows) {
+                while($row = $result->fetch_assoc()){
+                    $chartResult .= "<div class = 'titleBar ".$row['ClassID']."' style = 'display:none;'><hr>";
+                    $chartResult .= "<span>Class ID: " . $row['ClassID'] . " | Class Name: " . $row['ClassName'] . " | Term: " . $row['Term'] . '</span><br>';
+                    $chartResult .= "<hr></div>";
+                    $serializedData = $row['Layout'];
                     $layoutArr = unserialize($row['Layout']);
-
                     $chartResult .= "<div class = 'chartTable'>";
-                    $chartResult .= "<br><br><table class=\"table table-bordered ".$row["ClassID"]."\" style = 'display: none;'>";
-
+                    $chartResult .= "<table class=\"table table-bordered ".$row["ClassID"]."\" style = 'display: none;'>";
                     $numOfColumns = sizeof($layoutArr[0]);
                     $numOfRows = sizeof($layoutArr);
                     for ($row = 0; $row < $numOfRows; $row++){
@@ -66,13 +60,12 @@ require "includes".DIRECTORY_SEPARATOR."databaseConnection.php";
                     $chartResult .= "</table>";
                     $chartResult .= "</div>";
                 }
-
                 echo $chartResult;
             }
-?>
-            </div>
+            ?>
         </div>
-    </div>
+</div>
+<div id="bottom"></div>
 </body>
 <?php require "includes".DIRECTORY_SEPARATOR."footer.php"?>
 </html>
